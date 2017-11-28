9c82e9cb587169e7450b3b855bf3433a81573dfb
Hack Reactor Student <student@hackreactor.com>
2017-11-28 10:54:56 -0800

    Fully implemented getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,40 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-document.addEventListener('DOMContentLoaded', function(event) {
   var getElementsByClassName = function(className) {
-    // your code here
     var result = [];
-
-    for (var divobj in document.body) {
-      if (divobj === className) {
-          //result.push divObj
+    var searchNodes = function (parentNode) {
+      if (parentNode.classList && parentNode.classList.contains(className)) {
+        result.push(parentNode);
       }
-    }
 
-    if (element.childNodes) {
-      getElementsByClassName(element.childNodes);
-    }
+      if (parentNode.childNodes) {
+        parentNode.childNodes.forEach(function (node) {
+          searchNodes(node);
+        });
+      }
+    };
 
-    if (document.body.someDiv /* contains className */) {
-        //result.push(document.body.someDiv);
-        // var body = document.body;
-    }
-
-      //console.log(document.body.slice(document.body.indexOf(className), document.body.indexOf(className) + className.length));
-    
-      // $('.className');
-
-    
-
-    //first: search the body for each element with a class of className
-    //then: if there are child nodes, call recursively on each
-    //on each 'hit,' push to result array
-
-
-    //use these in the code: document.body, element.childNodes, and element.classList
+    searchNodes(document.body);
 
     return result;
   };
-
-});
