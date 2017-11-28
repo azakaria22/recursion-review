fa39b5b8c91cf12b27e60f90a2e3ecaddc02506e
Hack Reactor Student <student@hackreactor.com>
2017-11-27 21:01:54 -0800

    Started skeleton code for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+    
+  $(document).ready(function () {
+
+    for (var divobj in document.body) {
+      if (divobj === className) {
+        //result.push divObj
+      }
+    }
+
+    if (document.body.someDiv /* contains className */) {
+      //result.push(document.body.someDiv);
+      // var body = document.body;
+    }
+
+    console.log(document.body.slice(document.body.indexOf(className), document.body.indexOf(className) + className.length));
+  
+    // $('.className');
+
+  
+
+  //first: search the body for each element with a class of className
+  //then: if there are child nodes, call recursively on each
+  //on each 'hit,' push to result array
+
+  });
+
+  //use these in the code: document.body, element.childNodes, and element.classList
+
+  return result;
 };
