a1f43d884ae62e48969496429c3c42fed8dc9784
Hack Reactor Student <student@hackreactor.com>
2017-11-27 21:16:40 -0800

    fleshed out skeleton for problem

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,36 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  // your code here
-  var result = [];
-    
-  $(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function(event) {
+  var getElementsByClassName = function(className) {
+    // your code here
+    var result = [];
 
     for (var divobj in document.body) {
       if (divobj === className) {
-        //result.push divObj
+          //result.push divObj
       }
     }
 
-    if (document.body.someDiv /* contains className */) {
-      //result.push(document.body.someDiv);
-      // var body = document.body;
+    if (element.childNodes) {
+      getElementsByClassName(element.childNodes);
     }
 
-    console.log(document.body.slice(document.body.indexOf(className), document.body.indexOf(className) + className.length));
-  
-    // $('.className');
+    if (document.body.someDiv /* contains className */) {
+        //result.push(document.body.someDiv);
+        // var body = document.body;
+    }
 
-  
+      //console.log(document.body.slice(document.body.indexOf(className), document.body.indexOf(className) + className.length));
+    
+      // $('.className');
 
-  //first: search the body for each element with a class of className
-  //then: if there are child nodes, call recursively on each
-  //on each 'hit,' push to result array
+    
 
-  });
+    //first: search the body for each element with a class of className
+    //then: if there are child nodes, call recursively on each
+    //on each 'hit,' push to result array
 
-  //use these in the code: document.body, element.childNodes, and element.classList
 
-  return result;
-};
+    //use these in the code: document.body, element.childNodes, and element.classList
+
+    return result;
+  };
+
+});
